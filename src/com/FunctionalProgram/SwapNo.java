9b0840abc4570e3c53228863f2c601a7a464291c
stutisaurya <stuti.saurya@gmail.com>
2021-06-20 20:51:37 +0530

    Swap Number

---
+++ src/com/FunctionalProgram/SwapNo.java
@@ -0,0 +1,17 @@
+package com.FunctionalProgram;
+
+public class SwapNo {
+
+	public static void main(String[] args) {
+		int temp;
+		int x = 100;
+		int y = 200;
+
+		temp = x;
+		x = y;
+		y = temp;
+
+		System.out.println("x = " + x + " and y = " + y);
+	}
+
+}
